a4621e64b1917fd6e099633817c09d9ba93698fd
FireLord22 <104571252+FireLord22@users.noreply.github.com>
2026-02-19 17:38:38 +0300

    Upd UI

---
+++ HardwareMonitor/Models/NetworkInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareMonitor.Models
+{
+    public class NetworkInfo
+    {
+        public string Name { get; set; }
+        public string MACAddress { get; set; }
+        public string Speed { get; set; }
+    }
+}
